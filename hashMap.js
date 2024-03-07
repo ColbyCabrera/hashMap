fbf93803cf35497108c78ae6b22df4b51873e11c
ColbyCabrera <gdthyispro@gmail.com>
2024-03-07 13:00:35 -0500

    Create get, has, remove, length, and clear methods

--- hashMap.js
+++ hashMap.js
@@ -33,13 +33,54 @@ class HashMap {
     let node = new Node(key, value);
 
     this.arr[bucket] = node;
+  }
 
-    console.log(this.arr[13]);
+  get(key) {
+    let bucket = this.hash(key) % this.arr.length;
+    return this.arr[bucket] ? this.arr[bucket] : null;
+  }
+
+  has(key) {
+    let bucket = this.hash(key) % this.arr.length;
+    return this.arr[bucket] ? true : false;
+  }
+
+  remove(key) {
+    let bucket = this.hash(key) % this.arr.length;
+    this.arr[bucket] = null;
+    return this.arr[bucket] ? true : false;
+  }
+
+  length() {
+    let length = 0;
+    this.arr.forEach(element => {
+      if (element != null) {
+        length++;
+      }
+
+    });
+
+    return length;
+  }
+
+  clear() {
+    for (let i = 0; i < this.arr.length; i++) {
+      this.arr[i] = null;
+    }
   }
 }
 
 let test = new HashMap();
-console.log(test.hash("Sara"));
-console.log(test.hash("raSa"));
 test.set("Sara", 5);
-test.set("raSa", 5);
+test.hash("zy", 5);
+console.log(test.has("Sara"));
+test.set("asara", 5);
+test.set("Sar46sda", 5);
+test.set("jttara", 5);
+console.log(test.length());
+test.clear();
+
+console.log(test.length());
+
+
+
